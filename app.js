fc11fcb4976241f860b6887c9efc964761cde44f
Gavin <Gavinlowa111989@gmail.com>
2022-12-21 11:24:57 +0800

    done partial CSS

--- app.js
+++ app.js
@@ -50,11 +50,30 @@ document.querySelector(
   "#dateTime"
 ).innerText = `Today is: ${convertReadableTime}`;
 
-function timeDifference(timestamp1, timestamp2) {
-  var difference = timestamp1 - timestamp2;
-  var minutesDifference = Math.floor(difference / 1000 / 60);
+// function timeDifference(timestamp1, timestamp2) {
+//   var difference = timestamp1 - timestamp2;
+//   var minutesDifference = Math.floor(difference / 1000 / 60);
 
-  return minutesDifference;
+//   return minutesDifference;
+// }
+
+function convertToAmPm(dateTimeString) {
+  // Create a new JavaScript Date object from the date time string
+  let date = new Date(dateTimeString);
+
+  // Get the hours and minutes from the date object
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  // Convert the hours to AM/PM format
+  let amPm = hours < 12 ? "AM" : "PM";
+  hours = hours % 12 || 12;
+
+  // Convert the minutes to a string with leading zeros if necessary
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  // Return the formatted string
+  return hours + ":" + minutes + " " + amPm;
 }
 
 // //----------------
@@ -101,11 +120,65 @@ function timeDifference(timestamp1, timestamp2) {
 // };
 
 // Navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-//----------------
 
-async function getBusFromBusStop() {
-  const element = document.getElementById("buscode");
-  const _buscode = element.value;
+//---------- Using Busstop Code, trigger event listener's function.
+
+// event trigger
+document
+  .getElementById("buscodeButton")
+  .addEventListener("click", getBusFromBusStop);
+
+////////////////////////////////////////
+// //   Search BusStops from Road Name
+////////////////////////////////////////
+
+async function getBusStopsFromRoad() {
+  // to clear previous screen
+  document.getElementById("location").innerHTML = "";
+  document.getElementById("display").innerHTML = "";
+
+  // to get the input value
+  const element = document.getElementById("RoadInput");
+  const roadNameInput = element.value;
+
+  // create a div container to hold all the <ul> and <il> inside
+  const stopsAlongRoad = document.createElement("div");
+  stopsAlongRoad.setAttribute("id", "stopsAlongRoad");
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "stopsAlongRoadList");
+
+  for (i = 0; i < allBusStops.length; i++) {
+    if (allBusStops[i].RoadName == roadNameInput) {
+      const li = document.createElement("li");
+      li.setAttribute("class", "rdDesc");
+      li.setAttribute("value", `${allBusStops[i].BusStopCode}`);
+      li.innerText = allBusStops[i].Description;
+      // li.addEventListener(
+      //   "click",
+      //   getBusFromBusStop(allBusStops[i].BusStopCode)
+      // );
+      li.addEventListener("click", getBusFromBusStop);
+      ul.append(li);
+    }
+  }
+  stopsAlongRoad.append(ul);
+  document.querySelector("#display").append(stopsAlongRoad);
+}
+
+////////////////////////////////////////////////////
+// //   Function to get Bus Services from Bus Stop
+////////////////////////////////////////////////////
+
+async function getBusFromBusStop(e) {
+  let _buscode;
+  if (e.target.id == "buscodeButton") {
+    // check if the target i choose, if the id, is buscode, then assign _buscode to the the input value
+    _buscode = document.getElementById("buscode").value;
+  } else {
+    // else, instantiate _buscode to be the item which is the "value" of the target element.
+    _buscode = e.target.value;
+  }
 
   // clear upon every refresh
   document.getElementById("location").innerHTML = "";
@@ -155,21 +228,27 @@ async function getBusFromBusStop() {
         const ul_inner = document.createElement("ul");
         ul_inner.setAttribute("id", "busArrival");
 
+        // loops through and Add subsequent 3 bus timings
         const li_inner1 = document.createElement("li");
         li_inner1.setAttribute("class", "busArrivalList");
-        li_inner1.innerText = json.Services[i].NextBus.EstimatedArrival;
+        const busTime1 = json.Services[i].NextBus.EstimatedArrival;
+        li_inner1.innerText = convertToAmPm(busTime1);
         ul_inner.append(li_inner1);
 
         const li_inner2 = document.createElement("li");
         li_inner2.setAttribute("class", "busArrivalList");
-        li_inner2.innerText = json.Services[i].NextBus2.EstimatedArrival;
+        const busTime2 = json.Services[i].NextBus2.EstimatedArrival;
+        li_inner2.innerText = convertToAmPm(busTime2);
+        // li_inner2.innerText = json.Services[i].NextBus2.EstimatedArrival;
         ul_inner.append(li_inner2);
 
         const li_inner3 = document.createElement("li");
         li_inner3.setAttribute("class", "busArrivalList");
-        li_inner3.innerText = json.Services[i].NextBus3.EstimatedArrival;
+        const busTime3 = json.Services[i].NextBus3.EstimatedArrival;
+        li_inner3.innerText = convertToAmPm(busTime3);
         ul_inner.append(li_inner3);
 
+        // after looping, add the data into html format, and appending it to the existing "containers" to display contents
         li.append(ul_inner);
         ul.appendChild(li);
       }
@@ -179,45 +258,4 @@ async function getBusFromBusStop() {
     .catch((error) => {
       console.error("Error:", error);
     });
-}
-
-// getBusFromBusStop("13062");
-
-////// --- Park aside
-// async function clickBusCode() {
-//   const element = document.getElementById("buscode");
-//   console.log(element.value);
-//   console.log(typeof element.value);
-//   // const result = await Promise.resolve(getBusFromBusStop(element.value));
-//   // console.log(result);
-// }
-
-/// --------------------- Search BusStops from Road Name
-
-async function getBusStopsFromRoad() {
-  // to clear previous screen
-  document.getElementById("location").innerHTML = "";
-  document.getElementById("display").innerHTML = "";
-
-  // to get the input value
-  const element = document.getElementById("RoadInput");
-  const roadNameInput = element.value;
-
-  // create a div container to hold all the <ul> and <il> inside
-  const stopsAlongRoad = document.createElement("div");
-  stopsAlongRoad.setAttribute("id", "stopsAlongRoad");
-
-  const ul = document.createElement("ul");
-  ul.setAttribute("id", "stopsAlongRoadList");
-
-  for (i = 0; i < allBusStops.length; i++) {
-    if (allBusStops[i].RoadName == roadNameInput) {
-      const li = document.createElement("li");
-      li.setAttribute("class", "rdDesc");
-      li.innerText = allBusStops[i].Description;
-      ul.append(li);
-    }
-  }
-  stopsAlongRoad.append(ul);
-  document.querySelector("#display").append(stopsAlongRoad);
 }
